6e2fc39af4671e1a891eedc648c235f334cebb65
PasiKl <pasi.kleimola@gmail.com>
2022-05-23 00:44:05 +0300

    Add application quit.

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -14,7 +14,8 @@ public class StartGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
     }
 
     public void Begin()
